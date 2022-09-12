f3b71fe99e75a84841cb61fcef1434273a7f7c76
Igor Brizack <igor_brizack@hotmail.com>
2022-09-12 15:41:34 -0300

    sales service teste

--- tests/unit/services/sales.service.test.js
+++ tests/unit/services/sales.service.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 const salesService = require('../../../src/services/sales.service')
-// const salesModel = require('../../../src/models/sales.model')
+const salesModel = require('../../../src/models/sales.model')
 
 const sinon = require('sinon');
 
@@ -10,6 +10,9 @@ describe('Teste unitário do salesService', async function () {
   afterEach(sinon.restore)
 
   it('Inserindo uma venda com sucesso', async function () {
+    sinon.stub(salesModel, 'startSale').resolves(3);
+    sinon.stub(salesModel, 'insert').resolves({ id: 3, itemsSold: insertSucces.message.itemsSold });
+
 
     const result = await salesService.insertSales([
       {
@@ -23,5 +26,20 @@ describe('Teste unitário do salesService', async function () {
     ]);
 
     expect(result).to.deep.equal(insertSucces)
+  })
+
+  it('passando parâmetros errado para a minha requisição', async function () {
+      const result = await salesService.insertSales([
+      {
+        productI: 1,
+        quantity: 1
+      },
+      {
+        productId: 2,
+        quantity: 5
+      }
+    ]);
+
+    expect(result).to.deep.equal({ type: 'PRODUCT_IS_INVALID', message: '"productId" is required' })
   })
 })
